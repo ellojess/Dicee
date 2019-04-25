4baae46d3591faa5af67fe4dcf483c87a0d9f927
Jessica Trinh <jtrinh619@gmail.com>
2019-04-24 18:08:17 -0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Bo on 4/24/19.
+//  Copyright © 2019 AppBrewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
